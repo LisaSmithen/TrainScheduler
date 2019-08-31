2b23c3128fdeac8bef0aee5c4015c275fb4502ee
Lisa Smithen <smithenlisa@gmail.com>
2019-08-31 17:24:37 -0500

    Psuedocoding was added to file to lay framework for javascript coding

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,20 @@
+//Attach Firebase vars config thingy
+
+
+
+//Initialize Firebase app
+
+//Create a var database
+
+//Create the onclick event vars
+
+
+//Add train info onclick function to store and retrieve train data
+
+//Push train information to database
+
+//Snapshottime
+
+//Child added function using Snapshot and moment to show the difference between the train times
+
+//Append the destinantion, frequency, next train etc
