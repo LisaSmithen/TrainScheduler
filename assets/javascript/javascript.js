dc59826202c00102c21e6477114711d45e3aa10d
Lisa Smithen <smithenlisa@gmail.com>
2019-09-01 16:19:44 -0500

    Html skeleton added to index file. Javascript file updated with moment coding

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,16 +16,6 @@ firebase.initializeApp(firebaseConfig);
 //Create a var database
 var database = firebase.database();
 //Create the onclick event vars and grab user info function
-var firstTrainName;
-var destination;
-var firstTrainTime;
-var frequency = 0;
-
-
-
-
-//Add train info onclick function to store and retrieve train data
-
 
 $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
@@ -33,25 +23,42 @@ $("#add-train-btn").on("click", function(event) {
  firstTrainName = $("#train-name-input").val().trim();
  destinantion = $("#train-dest-input").val().trim();
  firstTrainTime = $("#first-time-input").val().trim();
- frequency = $("#freq-input").val().val().trim();
+ frequency = $("#freq-input").val().trim();
 
 
-
-
-//Push train information to database
 var newTrain = {
-    firstTrainName: firstTrainName,
-    destination: destination,
-    firstTrainTime: firstTrainTime,
-    frequency: frequency,
+trainName: firstTrainName,
+trainDestination: tdestination,
+trainTime: firstTrainTime,
+trainFrequency: tfrequency,
 };
 
+//Push train information to database
 database.ref().push(newTrain);
 
 
+
+
+});
 //Snapshottime
-
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 //Child added function using Snapshot and moment to show the difference between the train times
-
+var firstTrainName = childSnapshot.val().trainName;
+var tdestination = childSnapshot.val().trainDestination;
+var firstTraintime = childSnapshot.val().trainTime;
+var tfrequency = childSnapshot.val().trainFrequency;
 //Append the destinantion, frequency, next train etc
+
+var firstTime = 0;
+varfirstTimeUpdated = moment(firstTime, "HH:mm").subtract(1, "years");
+
+var diffTime = moment();
+varnewTime = moment().diff(moment(firstTimeUpdated), "minutes");
+
+var trainFreq;
+var tRemainder = diffTime % trainFreq;
+
+var tMinutesToTrain = trainFreq - tRemainder;
+
+var nextTrain = moment().add(tMinutesToTrain, "minutes");
 });
